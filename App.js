31f6b773557025ec64687ecee0da9781ae61d88b
Sakshi Bhende <103889378+sakshi-bhende@users.noreply.github.com>
2025-01-28 22:40:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductCrudApp from './component/ProductCrudApp';
+
+function App() {
+  return (
+    <div className="App">
+     <ProductCrudApp />
+    </div>
+  );
+}
+
+export default App;
